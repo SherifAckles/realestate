be9f46614b54dec64e3dfb7d8ba9432fed65dad6
nabesteron <49988463+nabesteron@users.noreply.github.com>
2023-07-27 14:36:15 -0500

    worked on the header and the hamburger menu

--- components/header.js
+++ components/header.js
@@ -1,41 +1,61 @@
-import Link from 'next/link'
+import Link from 'next/link';
+import { useState } from 'react';
 
-const Header = ({ hamburger }) => {
+const Header = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
-    <header className="self-stretch bg-gray-white h-[98px] flex flex-row py-[22px] px-20 box-border items-center justify-center sticky w-full top-[0] [background:white] z-[2] text-center text-5xl text-primary-500 font-body-large-400 lg:pl-10 lg:pr-10 lg:box-border md:pl-6 md:pr-6 md:box-border">
+    <header className=" bg-white h-[98px] flex flex-row py-[22px] px-20 box-border items-center justify-center sticky w-full top-[0] z-[2] text-center text-5xl text-primary-500 font-body-large-400 lg:pl-10 lg:pr-10 lg:box-border md:pl-6 md:pr-6 md:box-border">
       <div className="flex-1 flex flex-row items-center justify-between">
-        <Link href='/' className="flex flex-row items-center justify-center gap-[8px]">
-          <img className="relative w-11 h-11" alt="" src="/houseline.svg" />
-          <div className="flex flex-col items-start justify-start">
-            <div className="relative leading-[24px] font-semibold">REIS</div>
-            <div className="relative text-sm leading-[16px] font-medium">
-              Real State
+        <Link href="/">
+          <div className="flex flex-row items-center justify-center gap-[8px]">
+            <img className=" w-11 h-11" alt="" src="/houseline.svg" />
+            <div className="flex flex-col items-start justify-start">
+              <div className=" leading-[24px] font-semibold">REIS</div>
+              <div className=" text-sm leading-[16px] font-medium">
+                Real Estate
+              </div>
             </div>
           </div>
         </Link>
-        <div className="flex flex-row items-center justify-end gap-[36px] text-sm text-primary-900 sm:flex">
-          <div className="flex flex-row items-start justify-start gap-[30px] lg:hidden ">
-            <div className="relative leading-[22px]">HOME</div>
-            <div className="relative leading-[22px]">ABOUT US</div>
-            <div className="relative leading-[22px]">OUR AGENTS</div>
-            <div className="relative leading-[22px] font-semibold">
-              PROPERTIES
-            </div>
-            <div className="relative leading-[22px]">GALLERY</div>
-            <div className="relative leading-[22px]">BLOG</div>
-            <div className="relative leading-[22px]">CONTACT US</div>
-            <div className="relative leading-[22px]">SEARCH</div>
-          </div>
-          {hamburger && (
-            <button className="cursor-pointer [border:none] p-0 bg-[transparent] hidden flex-row items-center justify-center lg:flex">
-              <img
-                className="relative w-6 h-6 overflow-hidden shrink-0 md:flex"
-                alt=""
-                src="/notification.svg"
-              />
-            </button>
-          )}
+        {/* navigation links */}
+        <div className="flex md:hidden  gap-4 ">
+          <Link href="/">
+            <div className="text-primary-900 cursor-pointer">Home</div>
+          </Link>
+          <Link href="/about">
+            <div className="text-primary-900 cursor-pointer">About</div>
+          </Link>
+          <Link href="/contact">
+            <div className="text-primary-900 cursor-pointer">Contact</div>
+          </Link>
         </div>
+        <button
+          onClick={toggleMenu}
+          className="hidden md:flex  cursor-pointer border-none p-0 bg-transparent flex flex-row items-center justify-center"
+        >
+          <img
+            className="w-6 h-6 overflow-hidden flex "
+            alt=""
+            src="/notification.svg"
+          />
+        </button>
+      </div>
+      {/* Responsive Hamburger navigation links Menu */}
+      <div className={` ${menuOpen ? 'block' : 'hidden'} absolute top-[98px] left-[20px] right-[20px] bg-white border rounded shadow`}>
+        <Link href="/">
+          <div className="text-primary-900 cursor-pointer p-4">Home</div>
+        </Link>
+        <Link href="/about">
+          <div className="text-primary-900 cursor-pointer p-4">About</div>
+        </Link>
+        <Link href="/contact">
+          <div className="text-primary-900 cursor-pointer p-4">Contact</div>
+        </Link>
       </div>
     </header>
   );
