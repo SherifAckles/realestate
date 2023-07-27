bde4e3715220b9798dc09cccbadad3e324e09f01
nabesteron <49988463+nabesteron@users.noreply.github.com>
2023-07-27 15:10:38 -0500

    centered nav links on lg, add hover effect

--- components/header.js
+++ components/header.js
@@ -8,38 +8,45 @@ const Header = () => {
     setMenuOpen(!menuOpen);
   };
 
+  
+  const navigationLinks = [
+    { text: 'Home', href: '/' },
+    { text: 'About', href: '/about' },
+    { text: 'Contact', href: '/contact' },
+  ];
+
   return (
-    <header className=" bg-white h-[98px] flex flex-row py-[22px] px-20 box-border items-center justify-center sticky w-full top-[0] z-[2] text-center text-5xl text-primary-500 font-body-large-400 lg:pl-10 lg:pr-10 lg:box-border md:pl-6 md:pr-6 md:box-border">
+    <header className="bg-white h-[98px] flex flex-row py-[22px] px-20 box-border items-center justify-center sticky w-full top-[0] z-[2] text-center text-5xl text-primary-500 font-body-large-400 lg:pl-10 lg:pr-10 lg:box-border md:pl-6 md:pr-6 md:box-border">
       <div className="flex-1 flex flex-row items-center justify-between">
         <Link href="/">
           <div className="flex flex-row items-center justify-center gap-[8px]">
-            <img className=" w-11 h-11" alt="" src="/houseline.svg" />
+            <img className="w-11 h-11" alt="" src="/houseline.svg" />
             <div className="flex flex-col items-start justify-start">
-              <div className=" leading-[24px] font-semibold">REIS</div>
-              <div className=" text-sm leading-[16px] font-medium">
+              <div className="leading-[24px] font-semibold">REIS</div>
+              <div className="text-sm leading-[16px] font-medium">
                 Real Estate
               </div>
             </div>
           </div>
         </Link>
         {/* navigation links */}
-        <div className="flex md:hidden  gap-4 ">
-          <Link href="/">
-            <div className="text-primary-900 cursor-pointer">Home</div>
-          </Link>
-          <Link href="/about">
-            <div className="text-primary-900 cursor-pointer">About</div>
-          </Link>
-          <Link href="/contact">
-            <div className="text-primary-900 cursor-pointer">Contact</div>
-          </Link>
+        <div className="flex md:hidden gap-10 mx-auto" >
+          {/* Use the map function to generate the navigation links */}
+          {navigationLinks.map((link) => {
+            const{href,text}=link
+           return <Link key={link} href={href}>
+              <div className="text-primary-900 cursor-pointer hover:scale-110 transition-transform">
+                {text}
+              </div>
+            </Link>
+          })}
         </div>
         <button
           onClick={toggleMenu}
-          className="hidden md:flex  cursor-pointer border-none p-0 bg-transparent flex flex-row items-center justify-center"
+          className="hidden md:flex cursor-pointer border-none p-0 bg-transparent flex flex-row items-center justify-center"
         >
           <img
-            className="w-6 h-6 overflow-hidden flex "
+            className="w-6 h-6 overflow-hidden flex"
             alt=""
             src="/notification.svg"
           />
@@ -47,15 +54,14 @@ const Header = () => {
       </div>
       {/* Responsive Hamburger navigation links Menu */}
       <div className={` ${menuOpen ? 'block' : 'hidden'} absolute top-[98px] left-[20px] right-[20px] bg-white border rounded shadow`}>
-        <Link href="/">
-          <div className="text-primary-900 cursor-pointer p-4">Home</div>
-        </Link>
-        <Link href="/about">
-          <div className="text-primary-900 cursor-pointer p-4">About</div>
-        </Link>
-        <Link href="/contact">
-          <div className="text-primary-900 cursor-pointer p-4">Contact</div>
-        </Link>
+        {/* Use the map function to generate the navigation links */}
+        {navigationLinks.map((link) => (
+          <Link key={link.href} href={link.href}>
+            <div className="text-primary-900 cursor-pointer p-4">
+              {link.text}
+            </div>
+          </Link>
+        ))}
       </div>
     </header>
   );
